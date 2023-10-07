c44832c1d2f53aeae618ede93e8fe0de80ae2b16
Hunter Suttle <huntermsuttle.tfa@gmail.com>
2023-10-07 17:19:25 -0600

    completed programming tasks w04

---
+++ scripts/w04-task.js
@@ -0,0 +1,86 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: "Hunter Suttle",
+    photo: '/ProfessionalPhoto3.0.jpg',
+    favoriteFoods: ['tacos', 'sushi', 'pasta', 'burgers', 'cookies'],
+    hobbies: ['investing', 'basketball', 'cooking', 'entrepreneurship'],
+    placesLived: []
+};
+
+/* Populate Profile Object with placesLive objects */
+
+myProfile.placesLived.push(
+    {
+        place: 'Puyallup, WA',
+        length: '15 years'
+    }
+);
+
+myProfile.placesLived.push(
+    {
+        place: 'Sumner, WA',
+        length: '4 years'
+    }
+);
+
+myProfile.placesLived.push(
+    {
+        place: 'Salt Lake City, UT',
+        length: '2 years'
+    }
+);
+
+myProfile.placesLived.push(
+    {
+        place: 'South Hill, WA',
+        length: '1 year'
+    }
+);
+
+myProfile.placesLived.push(
+    {
+        place: 'Rexburg, ID',
+        length: '3 years'
+    }
+);
+
+/* DOM Manipulation - Output */
+
+/* Name */
+document.querySelector('#name').textContent = myProfile.name;
+
+/* Photo with attributes */
+let img = document.querySelector('#photo');
+img.src = myProfile.photo;
+img.alt = myProfile.name;
+
+/* Favorite Foods List*/
+myProfile.favoriteFoods.forEach(food => {
+    let li = document.createElement('li');
+    li.textContent = food;
+    document.querySelector('#favorite-foods').appendChild(li);
+  });
+
+/* Hobbies List */
+var ul = document.querySelector("#hobbies");
+
+myProfile.hobbies.forEach(hobby => {
+  let li = document.createElement("li");
+  li.textContent = hobby;
+  ul.append(li);
+});
+
+/* Places Lived DataList */
+let dl = document.querySelector("#places-lived");
+
+myProfile.placesLived.forEach(place => {
+    let dt = document.createElement("dt");
+    dt.textContent = place.place;
+    dl.append(dt);
+  
+    let dd = document.createElement("dd");
+    dd.textContent = place.length;
+    dl.append(dd);
+  });
